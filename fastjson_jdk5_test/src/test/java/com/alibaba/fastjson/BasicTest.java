32f8a95a7f142415f29ca8a421a18575ea43358c
高铁 <shaojin.wensj@alibaba-inc.com>
2020-06-25 17:55:58 +0800

    add jdk5 testcase

--- fastjson_jdk5_test/src/test/java/com/alibaba/fastjson/BasicTest.java
+++ fastjson_jdk5_test/src/test/java/com/alibaba/fastjson/BasicTest.java
@@ -2,15 +2,23 @@ package com.alibaba.fastjson;
 
 import junit.framework.TestCase;
 
+import java.util.Date;
+
 public class BasicTest extends TestCase {
     public void test_for_jdk5() throws Exception {
         VO vo = new VO();
+        vo.date = new Date();
 
         String str = JSON.toJSONString(vo);
+
+        System.out.println(str);
         JSON.parseObject(str, VO.class);
+
+
     }
 
     public static class VO {
         public int id;
+        public Date date;
     }
 }
